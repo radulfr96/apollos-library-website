9db506d1aa27c51ab209f6a50b4b129c01e58bf0
Wade James Russell <wados.russell70@gmail.com>
2020-01-17 22:20:04 +1100

    Added api controller for users

---
+++ MyLibrary.Website/Controllers/api/UserController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MyLibrary.Common.Responses;
+
+namespace MyLibrary.Website.Controllers.api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpGet("")]
+        public async Task<IActionResult> GetUsers()
+        {
+            var response = new GetUsersResponse();
+
+            try
+            {
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return new StatusCodeResult(StatusCodes.Status500InternalServerError);
+            }
+            return new StatusCodeResult(StatusCodes.Status500InternalServerError);
+        }
+    }
+}
