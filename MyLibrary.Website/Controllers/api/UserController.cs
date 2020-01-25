887068e97f72619d03db998b278228372518d9bd
Wade James Russell <wados.russell70@gmail.com>
2020-01-25 19:13:33 +1100

    Added login api endpoint

--- MyLibrary.Website/Controllers/api/UserController.cs
+++ MyLibrary.Website/Controllers/api/UserController.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
+using MyLibrary.Common.Requests;
 using MyLibrary.Common.Responses;
 using Newtonsoft.Json;
 using NLog;
@@ -17,14 +19,15 @@ namespace MyLibrary.Website.Controllers.api
     [ApiController]
     public class UserController : ControllerBase
     {
-        private IHttpClientFactory _clientFactory;
+        private HttpClient _httpClient;
         private IConfiguration _configuration;
         private Logger _logger = LogManager.GetCurrentClassLogger();
 
         public UserController(IHttpClientFactory clientFactory, IConfiguration configuration)
         {
-            _clientFactory = clientFactory;
             _configuration = configuration;
+            _httpClient = clientFactory.CreateClient(_configuration.GetSection("BaseApiUrl").Value);
+
         }
 
         [HttpGet("")]
@@ -32,9 +35,8 @@ namespace MyLibrary.Website.Controllers.api
         {
             try
             {
-                var restRequest = new HttpRequestMessage(HttpMethod.Get, _configuration.GetSection("BaseApiUrl").Value + "api/user");
-                var client = _clientFactory.CreateClient();
-                var restResponse = await client.SendAsync(restRequest);
+                var restRequest = new HttpRequestMessage(HttpMethod.Get, "api/user");
+                var restResponse = await _httpClient.SendAsync(restRequest);
 
                 if (restResponse.IsSuccessStatusCode)
                 {
@@ -45,6 +47,38 @@ namespace MyLibrary.Website.Controllers.api
             catch (Exception ex)
             {
                 _logger.Error(ex, "Unable to retreive users");
+                return new StatusCodeResult(StatusCodes.Status500InternalServerError);
+            }
+            return new StatusCodeResult(StatusCodes.Status500InternalServerError);
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] LoginRequest request)
+        {
+            try
+            {
+                var restRequest = new HttpRequestMessage(HttpMethod.Post, "api/user/login");
+                var content = new StringContent(JsonConvert.SerializeObject(request), Encoding.UTF8, "application/json");
+                restRequest.Content = content;
+                var restResponse = await _httpClient.SendAsync(restRequest);
+                LoginResponse response = JsonConvert.DeserializeObject<LoginResponse>(await restResponse.Content.ReadAsStringAsync());
+
+                if (response.StatusCode == HttpStatusCode.OK)
+                {
+                    return Ok(response);
+                }
+                else if (response.StatusCode == HttpStatusCode.Accepted)
+                {
+                    return Accepted();
+                }
+                else if (response.StatusCode == HttpStatusCode.BadRequest)
+                {
+                    return BadRequest();
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.Error(ex, "Unable to login user.");
                 return new StatusCodeResult(StatusCodes.Status500InternalServerError);
             }
             return new StatusCodeResult(StatusCodes.Status500InternalServerError);
