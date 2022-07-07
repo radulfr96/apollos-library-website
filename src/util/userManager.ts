72e834fd7707747230338842da0400553fdb863b
Wade James Russell <wados.russell70@gmail.com>
2022-07-07 19:35:11 +1000

    added config lookup

--- src/util/userManager.ts
+++ src/util/userManager.ts
@@ -1,12 +1,15 @@
 import { createUserManager } from 'redux-oidc';
 import { UserManagerSettings, WebStorageStateStore } from 'oidc-client';
+import ConfigHelper from '../config/configHelper';
+
+const configHelper = new ConfigHelper();
 
 const userManagerConfig: UserManagerSettings = {
     client_id: 'apolloslibrarywebapp',
     redirect_uri: `${window.location.protocol}//${window.location.hostname}${window.location.port ? `:${window.location.port}` : ''}/callback`,
     response_type: 'id_token token',
     scope: 'openid offline_access profile email username role apolloslibraryapi IdentityServerApi',
-    authority: 'https://localhost:5001',
+    authority: configHelper.idpUrl,
     silent_redirect_uri: `${window.location.protocol}//${window.location.hostname}${window.location.port ? `:${window.location.port}` : ''}/silent_renew.html`,
     automaticSilentRenew: true,
     filterProtocolClaims: true,
