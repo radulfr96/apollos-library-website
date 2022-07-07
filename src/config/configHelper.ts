c2055b1e32d3d4343743d1749794cfef3796033d
Wade James Russell <wados.russell70@gmail.com>
2022-07-07 17:54:16 +1000

    added url config for prod

--- src/config/configHelper.ts
+++ src/config/configHelper.ts
@@ -7,6 +7,9 @@ export default class ConfigHelper {
         if (process.env.NODE_ENV === 'development') {
             this.apiUrl = 'https://localhost:5004';
             this.idpUrl = 'https://localhost:5001';
+        } else if (process.env.NODE_ENV.toString() === 'production') {
+            this.apiUrl = 'https://webapi.apolloslibrary.com';
+            this.idpUrl = 'https://idp.apolloslibrary.com';
         } else {
             this.apiUrl = '';
             this.idpUrl = '';
