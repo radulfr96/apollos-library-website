2fa87adcca8d42e60489d6fa01b1c35d152fefe0
Wade James Russell <wados.russell70@gmail.com>
2022-07-10 21:47:14 +1000

    started adding screenshots and looking into order page issue

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,35 +1,41 @@
 import React from 'react';
-import { Grid, Link, Typography } from '@mui/material';
+import {
+    AppBar, Grid, Link, Toolbar, Typography,
+} from '@mui/material';
 
 const Footer = () => (
-    <Grid
+    <AppBar
+        position="fixed"
+        color="secondary"
         sx={{
-            position: 'fixed', bottom: 0, padding: '20px',
+            top: 'auto', bottom: 0,
         }}
-        component="footer"
-        container
     >
-        <Grid item xs={2} />
-        <Grid item xs={3} textAlign="left">
-            <Typography>
-                &copy; Apollo&apos;s Library 2022
-            </Typography>
-        </Grid>
-        <Grid item xs={2} textAlign="center">
-            <Typography>
-                <Link href="https://www.iubenda.com/terms-and-conditions/86495681" sx={{ textDecoration: 'none', color: '#000000' }}>Terms and Conditions</Link>
-            </Typography>
-            <Typography>
-                <Link href="https://www.iubenda.com/privacy-policy/86495681" target="_blank" sx={{ textDecoration: 'none', color: '#000000' }}>Privacy Policy</Link>
-            </Typography>
-        </Grid>
-        <Grid item xs={3} textAlign="right">
-            <Typography>
-                ABN: 25 338 116 207
-            </Typography>
-        </Grid>
-        <Grid item xs={2} />
-    </Grid>
+        <Toolbar>
+            <Grid container spacing={2}>
+                <Grid item xs={2} />
+                <Grid item xs={3} textAlign="left">
+                    <Typography>
+                        &copy; Apollo&apos;s Library 2022
+                    </Typography>
+                </Grid>
+                <Grid item xs={2} textAlign="center">
+                    <Typography>
+                        <Link href="https://www.iubenda.com/terms-and-conditions/86495681" sx={{ color: '#FFFFFF' }}>Terms and Conditions</Link>
+                    </Typography>
+                    <Typography>
+                        <Link href="https://www.iubenda.com/privacy-policy/86495681" target="_blank" sx={{ color: '#FFFFFF' }}>Privacy Policy</Link>
+                    </Typography>
+                </Grid>
+                <Grid item xs={3} textAlign="right">
+                    <Typography>
+                        ABN: 25 338 116 207
+                    </Typography>
+                </Grid>
+                <Grid item xs={2} />
+            </Grid>
+        </Toolbar>
+    </AppBar>
 );
 
 export default Footer;
